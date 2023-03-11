3dfc676781bdeea231a8f51b22cf544373cac600
Sgt_William <williampaes097@gmail.com>
2023-03-11 10:53:45 -0300

    Exercicio 02 | Sequencia de fibonnaci

---
+++ 02_ex_Fibonacci.js
@@ -0,0 +1,38 @@
+//Altere o numero abaixo
+const Numero = 13;
+
+let FibonacciObj = [0, 1, 1];
+let found = false;
+
+
+// Função para gerar os numberos de fibo
+function generateContentFibonacciNumber(num){
+
+    for(let i = 2; i <= num; i++){
+        FibonacciObj.push(FibonacciObj[i- 1] + FibonacciObj[i])
+    }
+}
+
+
+// Função para verificar o numero
+function verifyContentFibonacciNumber(num){
+
+    generateContentFibonacciNumber(num)
+
+    for(let currentFibo of FibonacciObj){
+
+        if(+num === currentFibo){
+            console.log(`✅ O numero ${num} foi encontrado na sequência de Fibonacci`)
+            found = true
+            break;
+        }
+    }
+
+        if(found === false){
+            console.log(`🚨 O numero ${num} NÂO foi encontrado na sequência de Fibonacci`)
+        }
+
+}
+
+
+verifyContentFibonacciNumber(Numero)
